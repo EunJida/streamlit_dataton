05e50d0b72d58a177cc5a81379686f43e9abf5df
EunJida <107987493+EunJida@users.noreply.github.com>
2022-08-25 11:23:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,13 @@ agree3 = st.checkbox('몸살')
 agree4 = st.checkbox('인후통')
 
 if agree1 or agree2 or agree3 or agree4:
-     st.markdown('코로나19 증상이 있군요! 검사가 필요할 것 같아요.')
+     st.markdown('코로나19 증상이 있군요! 코로나 검사가 필요할 것 같아요.')
 
 line_break()
 
-callout('')
+callout(['현재 집에 진단키트가 없으신가요?',
+        'COVID-19의 두드러진 증상은 기침과 호흡 곤란을 포함합니다.',
+        'AI 기술을 활용하여 기침 소리로부터 COVID-19에 대한 유용한 통찰력을 얻을 수 있습니다.',
+        '기침 소리로부터 COVID-19를 검출하는 새로운 진단 도구를 사용해보세요!'])
+
+line_break()
